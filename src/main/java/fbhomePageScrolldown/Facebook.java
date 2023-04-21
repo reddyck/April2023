40d50514b135cd51dabe0a2898ea95e2e830bfb1
reddyck <101711404+reddyck@users.noreply.github.com>
2023-04-21 13:34:37 +0530

    first_Push

---
+++ src/main/java/fbhomePageScrolldown/Facebook.java
@@ -0,0 +1,20 @@
+package fbhomePageScrolldown;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeTest;
+
+public class Facebook {
+static WebDriver driver;
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+	}
+
+		@BeforeTest
+		void chromeLaunch()
+		{
+			
+		}
+	
+
+}
